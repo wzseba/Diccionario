c577c5cb7a0e3c5204488b6c29cc62398b9cb383
wzseba <wzseba@gmail.com>
2024-10-21 22:56:32 -0300

    add: desarrollo de metodos en proceso

--- src/diccionario/Diccionario.java
+++ src/diccionario/Diccionario.java
@@ -1,5 +1,44 @@
 package diccionario;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 public class Diccionario {
 
+	private Map<String, String> diccionario;
+
+	public Diccionario() {
+		this.diccionario = new HashMap<String, String>();
+	}
+
+	public void nuevoPar(String esp, String ing) {
+		diccionario.put(esp, ing);
+	}
+
+	public String traduce(String esp) {
+		String traduccion = diccionario.get(esp);
+		if (traduccion != null) {
+			return traduccion;
+		}
+		throw new Error("palabra no encontrada");
+	}
+
+	public String palabraAleatoria() {
+
+		List<String> claves = new ArrayList<String>(diccionario.keySet());
+		Random random = new Random();
+
+		return claves.get(random.nextInt(claves.size()));
+	}
+
+	public String palabraAleatoreaConNum(int indice) {
+		return "";
+	}
+
+	public String primeraLetraTraduccion(String esp) {
+		return "1 letra en ing";
+	}
 }
